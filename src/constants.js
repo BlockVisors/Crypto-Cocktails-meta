52d9e78a2b9a236f373cb3e206f56fe1c2487f02
Dameon Green <dameongreen@gmail.com>
2019-11-13 21:06:50 -0500

    initial

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const HOST = 'https://crypto-cocktails.herokuapp.com/'
+
+module.exports = {
+  HOST
+}
---
+++ src/database.js
@@ -0,0 +1,72 @@
+module.exports = {
+  "1": {
+    "attributes": [
+      {
+      "trait_type": "color",
+      "value": "amber"
+      },
+      {
+      "trait_type": "spirit type",
+      "value": "bourbon/rye"
+      },
+      {
+      "trait_type": "inventor",
+      "value": "unknown"
+      },
+      {
+      "trait_type": "sweetness",
+      "value": 4
+      },
+      {
+      "trait_type": "mixing difficulty",
+      "value": 90.2
+      },
+      {
+      "trait_type": "glass-type",
+      "value": "Old-Fashioned"
+      },
+      {
+        "trait_type": "herbal",
+        "value": "no"
+        },
+      {
+      "display_type": "boost_number",
+      "trait_type": "aqua_power",
+      "value": 10
+      },
+      {
+      "display_type": "boost_percentage",
+      "trait_type": "stamina_increase",
+      "value": 5
+      },
+      {
+      "display_type": "number",
+      "trait_type": "generation",
+      "value": 1
+      }
+      ],
+    "name": "Old Fashioned",
+    "image": "https://",
+    "origin": "10-18",
+    "description": "old-school drink whiskey forward",
+    "background_color": "#ffffff",
+    "external_url": "https://cryptococktails.co",
+
+  },
+  "2": {
+    "name": "Flerp Derp",
+    "birthday": "01-08"
+  },
+  "3": {
+    "name": "Buttle McFuddle",
+    "birthday": "01-22"
+  },
+  "4": {
+    "name": "Quiero Pancakes",
+    "birthday": "03-04"
+  },
+  "5": {
+    "name": "Guino the Penguin",
+    "birthday": "03-09"
+  }
+}
